6f6381663ad3e77fcda3fa05e68f5970ab9213af
dmotte <37443982+dmotte@users.noreply.github.com>
2023-07-05 20:32:51 +0200

    Improved Bash conditions and indentation

--- get.sh
+++ get.sh
@@ -6,9 +6,9 @@ cd "$(dirname "$0")"
 
 # Bash script to install or update raspi-safe-shutdown via web
 
-if [[ $EUID -ne 0 ]]; then
-	echo "This script must be run as root" 1>&2
-	exit 1
+if [ "$EUID" != "0" ]; then
+    echo "This script must be run as root" 1>&2
+    exit 1
 fi
 
 echo "Installing the required APT packages"
@@ -17,7 +17,7 @@ apt-get install -y python3-rpi.gpio
 
 echo "Downloading the raspi-safe-shutdown.service unit file"
 curl -Lo "/etc/systemd/system/raspi-safe-shutdown.service" \
-	https://raw.githubusercontent.com/dmotte/raspi-safe-shutdown/main/raspi-safe-shutdown.service
+    https://raw.githubusercontent.com/dmotte/raspi-safe-shutdown/main/raspi-safe-shutdown.service
 systemctl daemon-reload
 
 echo "Stopping the raspi-safe-shutdown service (if running)"
@@ -25,7 +25,7 @@ systemctl stop raspi-safe-shutdown
 
 echo "Downloading the raspi-safe-shutdown script"
 curl -Lo "/opt/raspi-safe-shutdown.py" \
-	https://raw.githubusercontent.com/dmotte/raspi-safe-shutdown/main/raspi-safe-shutdown.py
+    https://raw.githubusercontent.com/dmotte/raspi-safe-shutdown/main/raspi-safe-shutdown.py
 chmod +x "/opt/raspi-safe-shutdown.py"
 
 echo "Enabling raspi-safe-shutdown service at startup"
