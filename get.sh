92c8599da1f0edcd9c7de7ef20a47250e10ee5a2
dmotte <37443982+dmotte@users.noreply.github.com>
2023-10-04 00:53:46 +0200

    Improved get.sh

--- get.sh
+++ get.sh
@@ -2,36 +2,34 @@
 
 set -e
 
-cd "$(dirname "$0")"
-
 # Bash script to install or update raspi-safe-shutdown via web
 
-if [ "$EUID" != "0" ]; then
-    echo "This script must be run as root" 1>&2
+if [ "$EUID" != '0' ]; then
+    echo 'This script must be run as root' 1>&2
     exit 1
 fi
 
-echo "Installing the required APT packages"
+echo 'Installing the required APT packages'
 apt-get update
 apt-get install -y python3-rpi.gpio
 
-echo "Downloading the raspi-safe-shutdown.service unit file"
-curl -Lo "/etc/systemd/system/raspi-safe-shutdown.service" \
+echo 'Downloading the raspi-safe-shutdown.service unit file'
+curl -Lo /etc/systemd/system/raspi-safe-shutdown.service \
     https://raw.githubusercontent.com/dmotte/raspi-safe-shutdown/main/raspi-safe-shutdown.service
 systemctl daemon-reload
 
-echo "Stopping the raspi-safe-shutdown service (if running)"
+echo 'Stopping the raspi-safe-shutdown service (if running)'
 systemctl stop raspi-safe-shutdown
 
-echo "Downloading the raspi-safe-shutdown script"
-curl -Lo "/opt/raspi-safe-shutdown.py" \
+echo 'Downloading the raspi-safe-shutdown script'
+curl -Lo /opt/raspi-safe-shutdown.py \
     https://raw.githubusercontent.com/dmotte/raspi-safe-shutdown/main/raspi-safe-shutdown.py
-chmod +x "/opt/raspi-safe-shutdown.py"
+chmod +x /opt/raspi-safe-shutdown.py
 
-echo "Enabling raspi-safe-shutdown service at startup"
+echo 'Enabling raspi-safe-shutdown service at startup'
 systemctl enable raspi-safe-shutdown
 
-echo "Starting the raspi-safe-shutdown service"
+echo 'Starting the raspi-safe-shutdown service'
 systemctl start raspi-safe-shutdown
 
-echo "Installation completed successfully"
+echo 'Installation completed successfully'
